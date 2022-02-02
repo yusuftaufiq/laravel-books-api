2458035d679b166f681c4ee4a40d921f646b6c04
yt2951 <rahman.yusuftaufiq@gmail.com>
2022-02-02 20:04:22 +0700

    Add endpoints plan

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// api/v1/books
+// api/v1/books/{book}
+// api/v1/books?category={category}&language={language}
+// api/v1/categories
+// api/v1/categories/{category}/books?language={language}
+// api/v1/languages
+// api/v1/languages/{language}/books?category={category}
+// api/v1/search/{query}?language={language}
