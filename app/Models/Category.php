cfaf76c26f93024118cf6b69d88c46d13dee5ffc
yt2951 <rahman.yusuftaufiq@gmail.com>
2022-02-03 18:49:06 +0700

    Add get all categories implementation

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,6 +8,11 @@ final class Category
 {
     final public function all()
     {
-        // code...
+        $categories = collect(CategoryEnum::cases())->map(fn (CategoryEnum $category) => [
+            'slug' => $category->value,
+            'name' => $category->name(),
+        ]);
+
+        return $categories;
     }
 }
