a1c2a69631a37d7b78d8a62c0ffafdfa5c3803a0
yt2951 <rahman.yusuftaufiq@gmail.com>
2022-02-03 18:50:06 +0700

    Initial commit for Category Controller

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+final class CategoryController extends Controller
+{
+    final public function __construct(
+        private Category $category = new Category(),
+    )
+    {
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+}
