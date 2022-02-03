64db995cd2526c9917d92e3b30e7480311b7299e
yt2951 <rahman.yusuftaufiq@gmail.com>
2022-02-03 19:35:51 +0700

    Remove unused method

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -23,15 +23,4 @@ final class CategoryController extends Controller
     {
         return response($this->category->all(), Response::HTTP_OK);
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Category $category)
-    {
-        //
-    }
 }
