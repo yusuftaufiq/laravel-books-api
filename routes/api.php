7a48d21d295a1c0e8f001f22ceb4261cf324e137
yt2951 <rahman.yusuftaufiq@gmail.com>
2022-02-03 18:52:10 +0700

    Change imported classes to global facades

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,7 +11,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+
+\Route::middleware('auth:sanctum')->get('/user', function (\Request $request) {
     return $request->user();
 });
 
