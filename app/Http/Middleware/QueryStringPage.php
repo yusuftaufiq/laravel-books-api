b2efb076d49a572c618091ba7242d15af467351b
yt2951 <rahman.yusuftaufiq@gmail.com>
2022-02-04 17:26:37 +0700

    Initial commit for Query String Page Middleware

---
+++ app/Http/Middleware/QueryStringPage.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class QueryStringPage
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $queryStringPage = $request->query('page', 1);
+        $request->route()->setParameter('page', $queryStringPage);
+
+        return $next($request);
+    }
+}
