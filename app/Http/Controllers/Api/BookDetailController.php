9fc5c3a8e9d2f34387d9071303e183bbd10beca1
yt2951 <rahman.yusuftaufiq@gmail.com>
2022-02-05 11:16:58 +0700

    Remove unused methods

--- app/Http/Controllers/Api/BookDetailController.php
+++ app/Http/Controllers/Api/BookDetailController.php
@@ -17,49 +17,4 @@ class BookDetailController extends Controller
     {
         //
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\BookDetail  $bookDetail
-     * @return \Illuminate\Http\Response
-     */
-    public function show(BookDetail $bookDetail)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\BookDetail  $bookDetail
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, BookDetail $bookDetail)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\BookDetail  $bookDetail
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(BookDetail $bookDetail)
-    {
-        //
-    }
 }
