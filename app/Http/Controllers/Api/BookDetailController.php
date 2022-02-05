f6cddbc8910ec8092cb9de5528fdd981521d1cb7
yt2951 <rahman.yusuftaufiq@gmail.com>
2022-02-05 11:17:30 +0700

    Add book details response

--- app/Http/Controllers/Api/BookDetailController.php
+++ app/Http/Controllers/Api/BookDetailController.php
@@ -3,18 +3,25 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Book;
 use App\Models\BookDetail;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
-class BookDetailController extends Controller
+final class BookDetailController extends Controller
 {
+    final public function __construct(
+        private BookDetail $bookDetail,
+    ) {
+    }
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    final public function index(Book $book): Response
     {
-        //
+        return response($book->details(), Response::HTTP_OK);
     }
 }
