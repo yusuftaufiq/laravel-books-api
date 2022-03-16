1604519bbe194c9a2f60a2faf0cc0a9a14cd9dff
yt2951 <rahman.yusuftaufiq@gmail.com>
2022-03-16 20:00:10 +0700

    test: initial personal access token factory

---
+++ database/factories/PersonalAccessTokenFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\PersonalAccessToken>
+ */
+class PersonalAccessTokenFactory extends Factory
+{
+    public const DEFAULT_PLAIN_TEXT_TOKEN = 'NYMsLiXkCnPkIMt6lSZWL6X8EJsYaxmmfn2AJO8B';
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->sentence,
+            'token' => hash(algo: 'sha256', data: static::DEFAULT_PLAIN_TEXT_TOKEN),
+            'abilities' => ['*'],
+            'expired_at' => $this->faker->dateTimeBetween('today', '+1 month')->format('Y-m-d'),
+        ];
+    }
+}
