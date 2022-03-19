bbfc7685bed02f1c5852cfdcc6c936b8c6a87b8e
yt2951 <rahman.yusuftaufiq@gmail.com>
2022-03-19 17:50:48 +0700

    test(unit): initial tests for book detail controller

---
+++ tests/Unit/Http/Controllers/BookDetailControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit\Http\Controllers;
+
+use App\Contracts\BookInterface;
+use App\Http\Controllers\Api\BookDetailController;
+use App\Http\Resources\BookResource;
+use Mockery\MockInterface;
+use Tests\TestCase;
+
+class BookDetailControllerTest extends TestCase
+{
+    public function testIndex(): void
+    {
+        $this->mock(BookInterface::class, function (MockInterface $mock): void {
+            $mock->shouldReceive('loadDetail')->once()->withNoArgs()->andReturnSelf();
+        });
+
+        /** @var BookDetailController */
+        $bookDetailController = $this->app->make(abstract: BookDetailController::class);
+        $bookWithDetail = $this->app->call([$bookDetailController, 'index']);
+
+        $this->assertInstanceOf(expected: BookResource::class, actual: $bookWithDetail);
+    }
+}
