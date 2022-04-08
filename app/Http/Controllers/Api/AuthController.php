9bc9b7b253bdb58c62d4a6bc304d2e28c24b29a9
yt2951 <rahman.yusuftaufiq@gmail.com>
2022-04-08 22:43:04 +0700

    try to trigger github workflow

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -14,61 +14,57 @@ use Symfony\Component\HttpFoundation\Response;
 
 final class AuthController extends Controller
 {
-    /**
-     * Create a new personal access token.
-     *
-     * @param \App\Http\Requests\LoginRequest $request
-     *
-     * @return \App\Http\Resources\PersonalAccessTokenResource
-     */
-    public function login(LoginRequest $request): PersonalAccessTokenResource
-    {
-        $request->authenticateOrFail();
+/**
+ * Create a new personal access token.
+ *
+ * @param \Illuminate\Http\Request $request
+ *
+ * @return \App\Http\Resources\PersonalAccessTokenResource
+ */
+public function login(LoginRequest $request): PersonalAccessTokenResource
+{
+$request->authenticateOrFail();
 
-        /** @var User $user */
-        $user = $request->user();
+$user = $request->user();
 
-        /** @var string $tokenName */
-        $tokenName = $request->input('token_name');
-        $expiredAt = $request->date(key: 'expired_at', format: 'Y-m-d');
+$tokenName = $request->input('token_name');
+$expiredAt = $request->date(key: 'expired_at', format: 'Y-m-d');
 
-        $token = $user->createExpirableToken(name: $tokenName, expiredAt: $expiredAt);
+$token = $user->createExpirableToken(name: $tokenName, expiredAt: $expiredAt);
 
-        $tokenResource = new PersonalAccessTokenResource($token->accessToken);
-        $tokenResource->withResponse($request, new JsonResponse(status: Response::HTTP_CREATED));
-        $tokenResource->additional([
-            'token' => [
-                'type' => 'Bearer',
-                'plain_text' => $token->plainTextToken,
-                'status' => TokenStatusEnum::Active,
-            ],
-        ]);
+$tokenResource = new PersonalAccessTokenResource($token->accessToken);
+$tokenResource->withResponse($request, new JsonResponse(status: Response::HTTP_CREATED));
+$tokenResource->additional([
+'token' => [
+'type' => 'Bearer',
+'plain_text' => $token->plainTextToken,
+'status' => TokenStatusEnum::Active,
+],
+]);
 
-        return $tokenResource;
-    }
+return $tokenResource;
+}
 
-    /**
-     * Log the user out (Invalidate the token).
-     *
-     * @param  \Illuminate\Http\Request $request
-     *
-     * @return \App\Http\Resources\PersonalAccessTokenResource
-     */
-    public function logout(Request $request): PersonalAccessTokenResource
-    {
-        /** @var User $user */
-        $user = $request->user();
+/**
+* Log the user out (Invalidate the token).
+*
+* @param  \Illuminate\Http\Request $request
+*
+* @return \App\Http\Resources\PersonalAccessTokenResource
+*/
+public function logout(Request $request): PersonalAccessTokenResource
+{
+$user = $request->user();
 
-        /** @var PersonalAccessToken $token */
-        $token = $user->currentAccessToken();
+$token = $user->currentAccessToken();
 
-        $token->delete();
+$token->delete();
 
-        return (new PersonalAccessTokenResource($token))->additional([
-            'token' => [
-                'type' => 'Bearer',
-                'status' => TokenStatusEnum::Revoked,
-            ],
-        ]);
-    }
+return (new PersonalAccessTokenResource($token))->additional([
+'token' => [
+'type' => 'Bearer',
+'status' => TokenStatusEnum::Revoked,
+],
+]);
+}
 }
